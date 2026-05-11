053b9ae0a242daa93b5eb182a6c47e8849c1b490
Ngọc Tiên <tien2k6v1@gmail.com>
2026-05-11 21:02:06 +0700

    feat: add English language support for HTML templates and controllers (#74)

--- src/main/java/com/sync/itk65/controller/AdminHopDongController.java
+++ src/main/java/com/sync/itk65/controller/AdminHopDongController.java
@@ -1,26 +1,27 @@
 package com.sync.itk65.controller;
 
-import com.sync.itk65.entity.HopDong;
-import com.sync.itk65.service.CanHoService;
-import com.sync.itk65.service.CuDanService;
-import com.sync.itk65.service.HopDongService;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.nio.charset.StandardCharsets;
+import com.sync.itk65.entity.HopDong;
+import com.sync.itk65.service.CanHoService;
+import com.sync.itk65.service.CuDanService;
+import com.sync.itk65.service.HopDongService;
 
 @Controller
 @RequestMapping("/admin/hop-dong")
